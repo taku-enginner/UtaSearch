2853050a26812df0f4497684219ba5ef64d820b0
taka4655 <taku.enginner@gmail.com>
2025-02-25 13:10:00 +0900

    render-build.shの追加

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,6 @@
 set -o errexit
 
-export NODE_OPTIONS=--openssl-legacy-provider
-
 bundle install
-yarn install # Yarnを使用している場合
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate

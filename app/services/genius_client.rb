9cbd5e9e459cd8bee255987ea8b3a9e5452a603b
taka4655 <taku.enginner@gmail.com>
2025-02-25 21:11:30 +0900

    リクエストにアクセストークンを設定#

--- app/services/genius_client.rb
+++ app/services/genius_client.rb
@@ -17,7 +17,6 @@ class GeniusClient
 
   def search_song(song_title, artist_name)
     response = self.class.get("/search", headers: @headers, query: { q: "#{song_title} #{artist_name}" })
-    # response = self.class.get("/search", query: { q: "#{song_title} #{artist_name}" })
     if response.success?
       JSON.parse(response.body)["response"]["hits"]
     else
@@ -35,28 +34,16 @@ class GeniusClient
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
     request = Net::HTTP::Get.new(uri.request_uri)
+    request["Authorization"] = @headers["Authorization"]  # アクセストークンを設定
     response = http.request(request)
-    parsed_html = Nokogiri::HTML(response)
-    
-    # 特定のdivタグの中身を抽出
-    lyrics_div = parsed_html.css('div[data-lyrics-container="true"]')
-    lyrics = lyrics_div.map(&:inner_html).join("\n")
-    
-    # if response.success?
-    #   # matches = response.body.scan(/<div data-lyrics-container="true" class="Lyrics-sc-3051f9b7-1 kLpNCS">(.*?)<\/div>/m)
-    #   response
-    #   # if response.any?
-    #   #   lyrics_array = matches.map { |match| match[0] } # マッチした内容を配列に格納
-    #   #   lyrics_array
-    #   # else
-    #   #   raise StandardError.new("Lyrics not found in the response body")
-    #   # end
-    # else
-    #   puts "Response Code: #{response.code}"
-    #   puts "Response Message: #{response.message}"
-    #   puts "Response Headers: #{response.headers.inspect}"
-    #   puts "Response Body: #{response.body}"
-    #   raise StandardError.new("Error: #{response.response}")
-    # end
+
+    if response.is_a?(Net::HTTPSuccess)
+      parsed_html = Nokogiri::HTML(response.body)
+      lyrics_div = parsed_html.css('div[data-lyrics-container="true"]')
+      lyrics = lyrics_div.map(&:inner_html).join("\n")
+      lyrics
+    else
+      raise StandardError.new("Error: #{response.code} #{response.message}")
+    end
   end
 end

37eec4399306ae9ea97f62979981a62d9c70041a
taka4655 <taku.enginner@gmail.com>
2025-02-25 20:43:21 +0900

    微修正

--- app/services/genius_client.rb
+++ app/services/genius_client.rb
@@ -30,7 +30,7 @@ class GeniusClient
   end
 
   def get_lyrics(url)
-    url = URI(url)
+    uri = URI(url)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
